92ab800717da6a8de29098a4fde653004d3c4f81
wahidfebr <wahidfebr@gmail.com>
2023-05-02 08:20:38 +0700

    feat: add home page intro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,17 @@
 export default function Home() {
-    return <h1 className="underline text-4xl">Ini home</h1>;
+    return (
+        <>
+            <div className="flex flex-col justify-start items-start pt-14 mx-80 w-1/4 gap-4">
+                <h1 className="text-3xl font-medium">
+                    <span className="animate-bounce">👋</span> Hi There!
+                    I&apos;m
+                </h1>
+                <h1 className="text-8xl font-extrabold">Wahid Febrilianto</h1>
+                <h1 className="text-2xl">
+                    A fullstack javascript developer from{" "}
+                    <span className="text-red-600">Indonesia</span>
+                </h1>
+            </div>
+        </>
+    );
 }
