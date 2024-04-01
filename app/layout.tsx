323bf1498140a55bf604943d7b876b4809afc9b3
wahidfebr <wahidfebr@gmail.com>
2024-04-01 10:48:01 +0700

    feat: add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
@@ -13,11 +14,14 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={true} attribute="class">
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
+        <>
+          <Analytics />
+          <ThemeProvider enableSystem={true} attribute="class">
+            <Navbar />
+            {children}
+            <Footer />
+          </ThemeProvider>
+        </>
       </body>
     </html>
   );
